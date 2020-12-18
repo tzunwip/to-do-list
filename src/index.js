6e1effd1a02c196f453e1e940d71bae53a57f52d
Tzun Wong <tzun.wong@gmail.com>
2020-12-19 00:36:07 +0800

    production build

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import { generateHome } from "./js/dom-gen-main";
 import { generateControls } from "./js/dom-gen-controls";
 import { generateFooter } from "./js/dom-gen-footer";
 import { createNewTaskForm, createModifyTaskForm, createSearchForm } from "./js/dom-gen-forms";
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+import "./css/normalize.css";
+import "./css/backgrounds.css";
+import "./css/main.css";
 
 const initialize = (() => {
   generateHome();
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,36 @@ module.exports = {
     filename: 'js/main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            }
+          }
+        ],
+      },
+      {
+        test: /\.(svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: "[name].[ext]",
+              outputPath: "svg/",
+            }
+          }
+        ],
+      },
+    ]
+  }
 };
